500f1204c71baeea5f8eb17aec023a17fd9d8a79
waltuh <programming1404@gmail.com>
2023-07-19 07:28:46 +0800

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+import "../scss/main.scss";
+import "animate.css";
+import { animate } from "./helper/animation";
+
+const loadingWrapper = document.querySelector(".loading-wrapper");
+const body = document.querySelector("body");
+
+Promise.all([
+  import("./components/toggle-theme"),
+  import("./components/theme"),
+  import("./components/menu"),
+])
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("hi");
+        animate(loadingWrapper, "fadeOut", 500);
+      }, 1000);
+    });
+  })
+  .then(() => {
+    loadingWrapper.style.display = "none";
+    return animate(body, "fadeIn");
+  });
